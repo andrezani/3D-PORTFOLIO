41cfbcf5bb7df9b38bbf3c26cd9e183a44183ada
Andrea <andrew.zani01@gmail.com>
2023-11-10 17:58:26 +0100

    Fix Navbar state and fix spacing in name display

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
 
   return (
     <nav
@@ -24,8 +24,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Andrea Zani{" "}
-            <span className="sm:block hidden">| Software Engineer</span>
+            Andrea Zani&nbsp;
+            <span className="sm:block hidden"> | Software Engineer</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
